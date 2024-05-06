efb912f4815a8de99ac4bf5f3d1eaa84ef7c3719
rdmccray <dennis@themccrays.net>
2024-05-06 03:20:31 -0500

    added some keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,6 +9,9 @@ keymap.set("n", "<leader>qq", ":q!<CR>") -- quit without saving
 keymap.set("n", "<leader>wf", ":w<CR>") -- save
 keymap.set("n", "<leader>Q", ":wqa<CR>") -- save and quit all
 keymap.set("n", "<leader>nh", "nohl<CR>", {desc = "Clear search highlights."})
+keymap.set("n", "<leader>o", "o<ESC>", {desc = "Insert line below without insert mode."})
+keymap.set("n", "<leader>O", "O<ESC>", {desc = "Insert line above without insert mode."})
+
 
 -- Split window management
 keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
