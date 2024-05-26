92c2265bb28b31aa6677b55e4230aa4f3c7dee21
rdmccray <101614786+rdmccray@users.noreply.github.com>
2024-05-25 20:48:18 -0500

    Update wezterm.lua config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,34 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
-
--- This will hold the configuration.
-local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.color_scheme = 'AdventureTime'
-config.enable_wayland = true
-
--- and finally, return the configuration to wezterm
-return config
+local wezterm = require("wezterm")
+return {
+    automatically_reload_config = true,
+    color_scheme = "Tokyonight",
+    enable_scroll_bar = false,
+    enable_tab_bar = false,
+    font_size = 14.0,
+    -- font = wezterm.font("MesloLGS NF", { weight = "Regular" }),
+    font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
+    -- window_background_image = "/home/dennis/.config/wallpapers/troncity.png",
+    window_background_opacity = 0.8,
+    window_padding = {
+        left = 2,
+        right = 2,
+        top = 5,
+        bottom = 0,
+    },
+    window_decorations = "RESIZE",
+    keys = {
+        {
+            key = "f",
+            mods = "CTRL",
+            action = wezterm.action.ToggleFullScreen,
+        },
+    },
+    mouse_bindings = {
+        -- Ctrl-click will open the link under the mouse cursor
+        {
+            event = { Up = { streak = 1, button = "Left" } },
+            mods = "CTRL",
+            action = wezterm.action.OpenLinkAtMouseCursor,
+        },
+    },
+}
